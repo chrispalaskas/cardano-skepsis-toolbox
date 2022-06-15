13aa82b0c535e113cf3c66893596b474b333f797
chrispalaskas <chris.palaskas@gmail.com>
2022-06-15 13:24:34 -0700

    remove comment

--- cardano_cli_helper.py
+++ cardano_cli_helper.py
@@ -252,7 +252,7 @@ def sendTokenToAddr(myPaymentAddrSignKeyFile: str, txInList: list, initLovelace:
     print ('Min fee:', fee)
     getRawTx(txInList, initLovelace, initToken, fromAddr, recipientList, ttlSlot, fee, minFee, tokenPolicyId, foreignTokensDict)
     signTx(myPaymentAddrSignKeyFile, network)
-    # return submitSignedTx()
+    return submitSignedTx()
 
 
 def getCnodeJournal(paymentAddr, tokenPolicyId, myTxHash):
