8436f46c7ddada2b1cb856aa72a4f03166eb0c99
Christos Palaskas <chris.palaskas@gmail.com>
2023-03-02 13:47:49 -0800

    Generate addr with or without funding

--- generateAddr.py
+++ generateAddr.py
@@ -1,9 +1,9 @@
 import cardano_cli_helper as cli
 import argparse
 from os.path import exists
+import sendTokens
 
-
-def main(fundingAddrFile, fundingSkeyFile, network='mainnet'):
+def generateAndFund(fundingAddrFile, fundingSkeyFile, network):
     if not exists(fundingAddrFile):
         print('ERROR: Funding address file does not exist.')
         return 0
@@ -11,12 +11,36 @@ def main(fundingAddrFile, fundingSkeyFile, network='mainnet'):
         print('ERROR: Funding skey file does not exist.')
         return 0
 
+    generateAccount(network)
+    sendTokens.main(fundingAddrFile, fundingSkeyFile, 'payment.addr', 100*pow(10,6),[],[],'testnet-magic 7', 'babbage-era')
+
+
+def generateAccount(network):
     cli.generatePaymentKeyPair()
     cli.generatePaymentAddress(network)
 
 
+def main(withFunding, lovelace_amount, fundingAddrFile, fundingSkeyFile, network='mainnet'):
+    if withFunding:
+        generateAndFund(fundingAddrFile, fundingSkeyFile, network)
+    else:
+        generateAccount(network)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-F', '--with-funding',
+                        default = True,
+                        dest = 'with_funding',
+                        help = 'Do you want to provide a funding key to fund your new account?',
+                        type = bool
+                        )
+    parser.add_argument('-L', '--lovelace-amount',
+                        default = 1000000,
+                        dest = 'lovelace_amount',
+                        help = 'Please provide the amount in lovelace to fund your new account',
+                        type = str
+                        )
     parser.add_argument('-A', '--funding-addr-file',
                     default = '/home/christos/IOHK/repos/cardano-node/running_block_producer/mambaQAPool/payment.addr',
                     dest='funding_addr_file',
@@ -30,13 +54,15 @@ if __name__ == '__main__':
                     type=str,
                     )
     parser.add_argument('-N', '--network',
-                    default='testnet-magic 9',
+                    default='testnet-magic 7',
                     dest='network',
                     help='Provide cardano network.',
                     type=str
                     )
     args = parser.parse_args()
 
-    main(args.funding_addr_file,
+    main(args.with_funding,
+         args.lovelace_amount,
+         args.funding_addr_file,
          args.funding_skey_file,
          args.network)
